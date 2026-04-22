634d4a969e3bfeb3ec62ed52ce2b21c3b17fbaaa
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-04-22 19:22:54 +0530

    ey

---
+++ strings/intro_to_string.py
@@ -0,0 +1,10 @@
+name="rabbani"
+print("The string name is :",name,"The length of the string is :",len(name))
+short_name=name[0:4] # it included the index 0 and excludes the index 4
+print(short_name)
+short_name=name[0:3] # it included the index 0 and excludes the index 3
+print(short_name)
+short_name=name[:] # starts from 0 to length of the string that is last letter
+print(short_name)
+short_name=name[1:] # starts from 1 to length of the string that is last letter
+print(short_name)
---
+++ strings/slicing_with_skipvalue.py
@@ -0,0 +1,6 @@
+name="rabbani"
+print("The string is ",name,"The length is :",len(name))
+print(name[0:5:3]) # skips the 3 values from starting of the index  
+# r  a  b  b  a  n  i
+# 0  1  2  3  4  5  6 (length is 7)
+# -7 -6 -5 -4 -3 -2 -1
