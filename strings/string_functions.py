0ab2db22a122cb45a257cee5f4e125fee704621e
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-04-23 10:24:56 +0530

    Create string_functions.py

---
+++ strings/string_functions.py
@@ -0,0 +1,14 @@
+name="rabbani"
+print("The length of the string is :",len(name))
+print(name.find("i"))
+print(name.replace("Rabbani","Rehaan"))
+print(name)
+print(name.capitalize())
+print(name)
+print(name.startswith("r"))
+print(name.endswith("i"))
+print(name[0:3])
+print(name[0:5])
+print(name[0:2])
+s = "hello"
+s[0] = "H"
